a006ae4b1fb20d4bdf95f320aa0f1cac7dff83b3
Dimitri63 <gruyer.dimitri@gmail.com>
2022-04-01 10:12:40 +0200

    first commit

---
+++ src/main/java/local/dgnex/hyperionstarsystem/HyperionStarSystemApplication.java
@@ -0,0 +1,13 @@
+package local.dgnex.hyperionstarsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperionStarSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperionStarSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/local/dgnex/hyperionstarsystem/HyperionStarSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package local.dgnex.hyperionstarsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperionStarSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
